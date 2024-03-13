0185f2ded3087984eefd8fb3b9278ac59cb456d7
Muhammad Siddiq <msiddiqshaikh10@gmail.com>
2024-03-13 22:30:43 +0500

    feat: models added

---
+++ src/models/user.model.js
@@ -0,0 +1,60 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true, // optimize way to set as true for search functionality
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, // url (cloudinary/ aws or any other service provider)
+      required: true,
+    },
+    coverImage: {
+      type: String, // url (cloudinary/ aws or any other service provider)
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModidified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+export const User = mongoose.model("User", userSchema);
