ff520ebd6cdbf95f6f82d041653762ef715551d9
Muhammad Siddiq <msiddiqshaikh10@gmail.com>
2024-03-13 20:50:27 +0500

    Initial commit : db config, utils: AsyncHandler, Api Response, Api Error

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+const PORT = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error connecting server", error);
+      throw error;
+    });
+    app.listen(PORT, () => {
+      console.log(`Server is running at port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed", err);
+  });
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,22 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+
+
+// using try catch
+
+// const asyncHandlers = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
